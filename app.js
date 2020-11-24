9041da7849c251e55280f470e1bcc72d9a9e6a32
Martina Scapola <martinascapola@gmail.com>
2020-11-23 23:39:34 -0300

    call to the api and connect with vue

---
+++ app.js
@@ -0,0 +1,40 @@
+const vue =  new Vue(
+    {
+        el: '.app',
+        data: {
+            champions: [], 
+            filterChampion: "", 
+            filteredChampions: []
+        },
+        methods: {
+            async getLolData() {
+                const lolData = await fetch("http://ddragon.leagueoflegends.com/cdn/10.23.1/data/en_US/champion.json")
+                .then((res) => res.json())
+                .then((data) => (obj = data))
+                .catch((error) => console.log(error));
+                const championsList = []
+
+                for (cham in lolData.data) {
+                    const champion = (lolData.data[cham])
+                    championsList.push(champion)
+                }      
+                this.champions = championsList
+                this.filteredChampions = championsList
+            },
+            filter() {       
+                const listFiltered = this.champions.filter((cham) => {
+                    let isfiltered = cham.name.toLowerCase().includes(this.filterChampion.toLowerCase())         
+                    return isfiltered
+                })
+                this.filteredChampions = listFiltered
+            }
+        },
+        mounted() {
+            this.getLolData()
+        },
+        watch: {
+            filterChampion() {
+                this.filter() 
+            },
+        }
+    });
